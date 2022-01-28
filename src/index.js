2284ed1c3f4ba4bb03c5f582d4873b7a89f61e8d
PauliVr <paulette.vr.10@gmail.com>
2022-01-27 22:00:21 -0600

    Documentación del código y Reorganización de elementos

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-let regExpNumber = /^\d{1,16}$/;
-let regExpName = /^([A-ZÑ]{1}[a-zñ]+[\s]*)+$/;
+let regExpNumber = /^\d{1,16}$/; //expresion regular para validar que en input tenga entre 1 hasta 16 digitos 
+let regExpName = /^([A-ZÑ]{1}[a-zñ]+[\s]*)+$/; //expresion regular que permita ingresar el nombre 
 
-if (document.querySelector('#name')) {
+if (document.querySelector('#name')) { //Bloque para validar los datos que entran al input Sí encuentra ese elemento en el HTML detecta que esta en la primer pág
   document.querySelector('#btn').addEventListener('click', function (e) {
-    e.preventDefault();
-    let cardName = document.querySelector('#name').value;
-    let cardNumber = document.querySelector('#card_number').value;
+    //seleccion del boton y le agregamos un evento que al pasar el evento click entre a la funcion
+    e.preventDefault(); //permite que no se recarge la página
+    let cardName = document.querySelector('#name').value; //seleccionamos el valor del input de nombre y lo asigno a la variale cardName
+    let cardNumber = document.querySelector('#card_number').value; //sellecciono el valor del input del numero de tarjeta y lo asigno a la variable cardNumber
     //console.log(cardName, cardNumber);
 
-    let cardNumberAlert = document.querySelector('#card-number-alert');
-    let cardNameAlert = document.querySelector('#card-name-alert');
+    let cardNumberAlert = document.querySelector('#card-number-alert'); //selecciono el contenedor para la alerta de del numero de tajeta invalido
+    let cardNameAlert = document.querySelector('#card-name-alert'); //selecciono el contenedor para la alerta de del nombre invalido
 
-    if (regExpName.test(cardName)) {
+    if (regExpName.test(cardName)) { //si el numero ingresado en el input cumple con la expresion regular y el test regresa un true 
       //let correctName = cardName;
-      cardNameAlert.classList.remove('red', 'margin');
-      cardNameAlert.innerHTML = ``;
-      localStorage.setItem('nameCard', cardName);
+      cardNameAlert.classList.remove('red', 'margin'); 
+      cardNameAlert.innerHTML = ``;  //quita texto y clases
+      localStorage.setItem('nameCard', cardName); //guarda en la memoria del navegador 
 
       // console.log(correctName);
-    } else {
+    } else { //bloque para ingresar alerta de invalido 
       cardNameAlert.innerHTML = `<p>nombre que ingresaste es inválido</p>`;
       cardNameAlert.classList.add('red', 'margin');
     }
@@ -27,41 +28,42 @@ if (document.querySelector('#name')) {
       //validamos que la entrada del usuario sea valida conla ER
       //let correctNumber = cardNumber; //guarda en correctNumber el numero que ingreso el usuario
       cardNumberAlert.classList.remove('red', 'margin');
-      cardNumberAlert.innerHTML = ``;
-      localStorage.setItem('numberCard', cardNumber);
+      cardNumberAlert.innerHTML = ``; //quita texto y clases
+      localStorage.setItem('numberCard', cardNumber); //guarda en la memoria del navegador  llave->valor para el set 
     } else {
+      //bloque para ingresar alerta de invalido
       cardNumberAlert.innerHTML = `<p> el numero de tarjeta es inválido </p>`;
       cardNumberAlert.classList.add('red', 'margin');
     }
 
-    if (regExpName.test(cardName) && regExpNumber.test(cardNumber)) {
-      window.location.href = 'card.html';
+    if (regExpName.test(cardName) && regExpNumber.test(cardNumber)) { //si ambos datos ingresados en el input cumplen la expresion regular 
+      window.location.href = 'card.html'; //cambiamos de ventana a card.html 
     }
   });
-} else {
-  let localNumber = localStorage.getItem('numberCard');
-  let isvalid = validator.isValid(localNumber);
+} else {  //si no detecta que esta en la primer pág por lo tanto esta en la segunda 
+  let localNumber = localStorage.getItem('numberCard'); //obtener el valor de el numero guardado en la memoria del navegador y asignarlo a la variable 
+  let isvalid = validator.isValid(localNumber); //mandamos el numero de tarjeta al objeto validator e invocamos el primer valor que contiene la funcion IsValid
   //console.log(isvalid);
 
-  let validAlert = document.querySelector('#isValidAlert');
+  let validAlert = document.querySelector('#isValidAlert'); //seleccionamos el contenedor para la aleta de si la tajeta es valida o no 
 
-  if (isvalid === true) {
-    validAlert.innerHTML = `<h3>Tajeta de crédito válida</h3> `;
-    validAlert.classList.add('isvalid');
+  if (isvalid === true) { //si al invocar la funcion nos regresa un verdadero 
+    validAlert.innerHTML = `<h3>Tajeta de crédito válida</h3> `; //agregamos el texto de la alerta --> valida
+    validAlert.classList.add('isvalid'); //agregamos su clase para el estilo 
   } else {
-    validAlert.innerHTML = `<h3>Tajeta de crédito inválida</h3> `;
-    validAlert.classList.add('isinvalid');
+    validAlert.innerHTML = `<h3>Tajeta de crédito inválida</h3> `; //agregamos el texto de la alerta -->invalida
+    validAlert.classList.add('isinvalid'); //agregamos su clase para el estilo
   }
 
-  let numberMaskify = localStorage.getItem('numberCard');
-  let maskify = validator.maskify(numberMaskify);
+  let numberMaskify = localStorage.getItem('numberCard'); //obtenemos el valor guardado en la memoria y lo asignamos en la variable 
+  let maskify = validator.maskify(numberMaskify); //mandamos el numero de tarjeta al objeto e invocamos la funcion 
 
   document.querySelector('#cardName').innerHTML =
-    localStorage.getItem('nameCard');
+    localStorage.getItem('nameCard'); //agrega el nombre ingresado en el input 
 
-  document.querySelector('#cardNumber').innerHTML = maskify;
+  document.querySelector('#cardNumber').innerHTML = maskify; //imprimimos el resultado de la funcion maskify 
   //console.log(maskify);
-  localStorage.clear();
+  localStorage.clear(); // limpiamos la memoria del navegador cuando hace todo el proceso 
 }
 
 import validator from './validator.js';
--- src/validator.js
+++ src/validator.js
@@ -1,64 +1,64 @@
 const validator = {
-  isValid: function isValid(creditCardNumber) {
-    let num = [];
-    let cardnum = [],
+  isValid: function isValid(creditCardNumber) {  //creamos la funcion como atributo del objeto 
+    let num = []; //creamos el primer arreglo 
+    let cardnum = [], 
       cardconvert = [],
       aux = []; //definimos arreglos auxiliares
     let flagA,
-      flagB,
+      flagB, 
       suma = 0; //definimos vaiables auxiliares
-    let valid = false;
+    let valid = false; //creamos una variable para los return
     //console.log('entrada del usuario' + creditCardNumber);
-    let array = creditCardNumber.split(''); // convertimos en arreglo el string del usuario
+    let array = creditCardNumber.split(''); // convertimos en arreglo el string del usuario y lo asignamos a array
     //console.log('Array' + array);
-    for (let i = creditCardNumber.length - 1; i >= 0; i--) {
+    for (let i = creditCardNumber.length - 1; i >= 0; i--) { //recorremos el arreglo desde la ultima posicion a la primera para invertilo 
       //recorre el arreglo desde el ultimo valor al primero
-      num.push(array[i]); //creamos el arreglo invertido
+      num.push(array[i]); //agregamos el valor al arreglo 
     }
     //console.log(num);
-    for (let i = 0; i < num.length; i++) {
+    for (let i = 0; i < num.length; i++) { //recorremos el arreglo desde 0 hasta su longitud
       //recorre el arreglo y hace las multiplicaciones
-      if (i % 2) {
-        num[i] = num[i] * 2;
-        if (num[i] >= 10) {
-          aux = num[i].toString();
-          flagA = aux.charAt(0);
-          flagB = aux.charAt(1);
-          cardnum[i] = parseInt(flagA) + parseInt(flagB);
-          cardconvert.push(cardnum[i]);
-        } else {
-          cardconvert.push(parseInt(num[i]));
+      if (i % 2) { //si la posicion es impar 
+        num[i] = num[i] * 2;  //multiplica el valor por 2 y asigna a su posicion 
+        if (num[i] >= 10) { //si el numero multiplicado es mayor a 10 
+          aux = num[i].toString(); //convertimos el valor a u string y lo guardamos en aux
+          flagA = aux.charAt(0); //asignamos el valor de la posicion 0 a flagA
+          flagB = aux.charAt(1); //asignamos el valor de la posicion 1 a flagB
+          cardnum[i] = parseInt(flagA) + parseInt(flagB); //guardamos en cardnum la suma de ambos digitos previamente convertidos a entero 
+          cardconvert.push(cardnum[i]); //agregamos el valor al arreglo final num 
+        } else { 
+          cardconvert.push(parseInt(num[i])); //si no es mayor a 10 se agrega al arreglo final num directamente 
         }
-      } else {
-        cardconvert.push(parseInt(num[i]));
+      } else { //si no es un valor impar
+        cardconvert.push(parseInt(num[i])); //agrega directamente el numero al arreglo final 
       }
       //console.log(cardconvert);
       //console.log(5);
 
-      suma = suma + cardconvert[i];
+      suma = suma + cardconvert[i]; //sumamos cada digito del arreglo 
       //console.log(suma);
     }
     if (suma >= 0 && suma <= 999) {
-      suma % 10 === 0 ? (valid = true) : (valid = false);
+      suma % 10 === 0 ? (valid = true) : (valid = false); //condicion -> suma sea multiplo de 10 if-> asigna un true else-> asigna un false 
     }
-    return valid;
+    return valid; //regresamos el valor de valid ya sea true o false 
   },
-  maskify: function maskify(creditCardNumber) {
-    let creditNumber = creditCardNumber.split('');
-    let array = [];
+  maskify: function maskify(creditCardNumber) { //creamos la funcion como valor del atributo del obj
+    let creditNumber = creditCardNumber.split(''); //creamos el numero un arreglo 
+    let array = []; 
     let res;
     //console.log(creditCardNumber);
 
-    for (let i = 0; i < creditNumber.length; i++) {
-      if (i >= 0 && i < creditNumber.length - 4) {
-        array.push('#');
-      } else {
-        array.push(creditNumber[i]);
+    for (let i = 0; i < creditNumber.length; i++) { //recorremos el arreglo hasta su longitud 
+      if (i >= 0 && i < creditNumber.length - 4) { //si la posicion es de 0 a la longitud menos 4
+        array.push('#'); //agremos al arreglo un #
+      } else { 
+        array.push(creditNumber[i]); //agreamos numero 
       }
     }
-    res = array.join('');
+    res = array.join(''); //le quita las , al arreglo y regresa string
 
-    return res;
+    return res; //retormanos el valor que tenga la variable 
   },
 };
 
